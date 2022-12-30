528c91a83857dfe112bce0e656d0c3a3367908cf
Luis Fernando Pedroza Taborda <luisefe387@hotmail.es>
2022-12-30 17:00:16 -0500

    Carga de todo el proyecto inicial

---
+++ src/test/java/com/mytrading/modules/zuul/ModulesZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytrading.modules.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulesZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
